18f0994beaadb8f4d84c5a318364c7fda1ab0aef
munnsmunns <mmunns16@gmail.com>
2018-02-23 16:12:00 -0600

    #6 Finished add-todo testing #5 Created submit for searching for an owner

---
+++ client/src/app/todos/add-todo.component.spec.ts
@@ -0,0 +1,40 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {MatDialogRef, MAT_DIALOG_DATA, MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+
+import {AddTodoComponent} from './add-todo.component';
+import {CustomModule} from '../custom.module';
+
+describe('Add todo component', () => {
+
+    let addTodoComponent: AddTodoComponent;
+    let calledClose: boolean;
+    const mockMatDialogRef = {
+        close() { calledClose = true; }
+    };
+    let fixture: ComponentFixture<AddTodoComponent>;
+
+    beforeEach(async( () => {
+        TestBed.configureTestingModule({
+            imports: [CustomModule],
+            declarations: [AddTodoComponent],
+            providers: [
+                { provide: MatDialogRef, useValue: mockMatDialogRef },
+                { provide: MAT_DIALOG_DATA, useValue: null },
+                { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }]
+        }).compileComponents().catch(error => {
+            expect(error).toBeNull();
+        });
+    }));
+
+    beforeEach(() => {
+        calledClose = false;
+        fixture = TestBed.createComponent(AddTodoComponent);
+        addTodoComponent = fixture.componentInstance;
+    });
+
+    it('closes properly', () => {
+        addTodoComponent.onNoClick();
+        expect(calledClose).toBe(true);
+    });
+});
